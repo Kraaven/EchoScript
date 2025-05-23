bda6f600d13b1fc399a2f7ec07c663a275cb8882
Kraaven <dhruv.bhojaraj@gmail.com>
2025-05-23 11:45:32 +0530

    expressions
    - Made local variables local to the function
    - Expressions are now purely evaluated with a stack

--- EchoScript/Program.cs
+++ EchoScript/Program.cs
@@ -5,8 +5,8 @@
     private static Dictionary<string, float> GlobalNumbers = new();
     private static Dictionary<string, string> GlobalStrings = new();
 
-    private static Dictionary<string, float> LocalNumbers = new();
-    private static Dictionary<string, string> LocalStrings = new();
+    // private static Dictionary<string, float> LocalNumbers = new();
+    // private static Dictionary<string, string> LocalStrings = new();
     
     public static void Main(String[] args)
     {
@@ -178,25 +178,33 @@
     public static void CallFunction(string functionName)
     {
         if (!FunctionLibrary.ContainsKey(functionName)) CrashError($"Function {functionName} does not exist");
-        LocalNumbers.Clear();
-        LocalStrings.Clear();
+        Dictionary<string, float> LocalNumbers = new();
+        Dictionary<string, string> LocalStrings = new();
         
         foreach (var Instruction in FunctionLibrary[functionName])
         {
+            //Variable Declaration
             switch (Instruction[0])
             {
                 case "num":
-                    if(Instruction.Length != 4) CrashError("Invalid Syntax, multiple arguments");
                     if(Instruction[2] != "=") CrashError("Invalid Syntax, no assigment operator");
                     
-                    if (float.TryParse(Instruction[3], out var numResult)) LocalNumbers.Add(Instruction[1], numResult);
-                    else CrashError("Invalid Syntax, argument must be a number");
+                    // if (float.TryParse(Instruction[3], out var numResult)) LocalNumbers.Add(Instruction[1], numResult);
+                    // else CrashError("Invalid Syntax, argument must be a number");
 
+                    string[] Expression = new string[Instruction.Length - 3];
+                    for (int i = 0; i < Expression.Length; i++)
+                    {
+                        Expression[i] = Instruction[i + 3];
+                    }
+                    
+                    LocalNumbers.Add(Instruction[1], EvaluateExpression(Expression, LocalNumbers, LocalStrings));
+                    // Console.WriteLine(string.Join("|", Expression));
                     // Console.WriteLine($"{Instruction[1]} added with value {LocalNumbers[Instruction[1]]}"); 
                     
                     continue;
                 case "str":
-                    if(Instruction.Length != 4) CrashError("Invalid Syntax, multiple arguments");
+                    if(Instruction.Length != 4) CrashError("Invalid Syntax, multiple tokens for string declaration");
                     if(Instruction[2] != "=") CrashError("Invalid Syntax, no assigment operator");
                     
                     LocalStrings.Add(Instruction[1], Instruction[3]);
@@ -204,7 +212,24 @@
                     // Console.WriteLine($"{Instruction[1]} added with value {LocalStrings[Instruction[1]]}");
 
                     continue;
+                
+                default:
+                    break;
+                
             }
+
+            if (LocalNumbers.ContainsKey(Instruction[0]) && Instruction[1] == "=")
+            {
+                string[] Expression = new string[Instruction.Length - 2];
+                for (int i = 0; i < Expression.Length; i++)
+                {
+                    Expression[i] = Instruction[i + 2];
+                }
+                LocalNumbers[Instruction[0]] = EvaluateExpression(Expression, LocalNumbers, LocalStrings);
+                
+                // Console.WriteLine($"{Instruction[0]} changed to value {LocalNumbers[Instruction[0]]}");
+            }
+            
         }
     }
 
@@ -214,6 +239,105 @@
         Console.WriteLine("Press any key to exit...");
         Console.ReadKey();
         Environment.Exit(0);
+    }
+    
+    public static float EvaluateExpression(string[] arguments, Dictionary<string, float> localNumbers, Dictionary<string, string> localStrings)
+    {
+        List<string> postfix = InfixToPostfix(arguments, localNumbers, localStrings);
+        return EvaluatePostfix(postfix);
+    }
+
+    private static readonly Dictionary<string, int> precedence = new Dictionary<string, int>
+        {
+            { "+", 1 },
+            { "-", 1 },
+            { "*", 2 },
+            { "/", 2 }
+        };
+    private static List<string> InfixToPostfix(string[] tokens, Dictionary<string, float> localNumbers, Dictionary<string, string> localStrings)
+    {
+        List<string> output = new List<string>();
+        Stack<string> operators = new Stack<string>();
+        
+        foreach (var token in tokens)
+        {
+            if (float.TryParse(token, out _))
+            {
+                output.Add(token);
+            }
+            else if (precedence.ContainsKey(token))
+            {
+                while (operators.Count > 0 && precedence.ContainsKey(operators.Peek()) &&
+                       precedence[operators.Peek()] >= precedence[token])
+                {
+                    output.Add(operators.Pop());
+                }
+                operators.Push(token);
+            }
+            else if (token == "(")
+            {
+                operators.Push(token);
+            }
+            else if (token == ")")
+            {
+                while (operators.Count > 0 && operators.Peek() != "(")
+                {
+                    output.Add(operators.Pop());
+                }
+                if (operators.Count == 0 || operators.Pop() != "(")
+                {
+                    throw new ArgumentException("Mismatched parentheses");
+                }
+            }
+            else if (localNumbers.ContainsKey(token))
+            {
+                output.Add(localNumbers[token].ToString());
+            }
+            else
+            {
+                CrashError("Invalid Syntax, expression has wrong sequence of operators or invalid varible calls");
+            }
+        }
+
+        while (operators.Count > 0)
+        {
+            var op = operators.Pop();
+            if (op == "(" || op == ")")
+            {
+                throw new ArgumentException("Mismatched parentheses");
+            }
+            output.Add(op);
+        }
+
+        return output;
+    }
+
+    private static float EvaluatePostfix(List<string> postfix)
+    {
+        Stack<float> stack = new Stack<float>();
+
+        foreach (string token in postfix)
+        {
+            if (float.TryParse(token, out float num))
+            {
+                stack.Push(num);
+            }
+            else
+            {
+                float right = stack.Pop();
+                float left = stack.Pop();
+                switch (token)
+                {
+                    case "+": stack.Push(left + right); break;
+                    case "-": stack.Push(left - right); break;
+                    case "*": stack.Push(left * right); break;
+                    case "/": stack.Push(left / right); break;
+                    default: throw new InvalidOperationException($"Invalid operator {token}");
+                }
+            }
+        }
+
+        return stack.Pop();
     }
 
 
